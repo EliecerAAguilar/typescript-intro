f4d78220b4e9bd6d137162e608fcf8c371e50220
Eliecer <eliecer.aguilar1@utp.ac.pa>
2024-06-10 21:44:34 -0500

    feat(topics)!: N18
    add interface functions types

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -4,21 +4,40 @@ function addNumbers(a: number, b: number){
 
 const result: number = addNumbers(2,3);
 
-console.log({result});
+// console.log({result});
 
 const addNumberArrow = (a: number, b: number): string => {
     return `${a + b}`;
 }
 
 const resultArrow: string = addNumberArrow(2, 3);
-console.log({resultArrow});
+// console.log({resultArrow});
 
 function multiply(firstNumber: number, secondNumber?: number, base: number = 2) {
     return firstNumber * base;
 }
 
 const resultMultiply = multiply(2);
-console.log({resultMultiply});
+// console.log({resultMultiply});
 
+interface Character {
+    name: string;
+    hp: number;
+    showHP: () => void;
+}
+
+const healCharacter = (character: Character, amount: number) => {
+    character.hp += amount;
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 50,
+    showHP() {
+        console.log(`Puntos de vida ${this.hp}`);
+    }
+}
+
+healCharacter(strider, 25);
 
 export {};
