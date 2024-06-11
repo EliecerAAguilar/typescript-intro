3c9543c621a0e0041aa7e8cdde98f861e1293d77
Eliecer <eliecer.aguilar1@utp.ac.pa>
2024-06-10 22:26:50 -0500

    feat(topics)!: N22
    add destructuring arrays

--- src/topics/05-basic-destructuring.ts
+++ src/topics/05-basic-destructuring.ts
@@ -20,18 +20,30 @@ const audioPlayer: AudioPlayer = {
     }
 }
 
-console.log('Song: ', audioPlayer.song);
-console.log('SongDuration: ', audioPlayer.songDuration);
-console.log('Author: ', audioPlayer.details.artist);
+// console.log('Song: ', audioPlayer.song);
+// console.log('SongDuration: ', audioPlayer.songDuration);
+// console.log('Author: ', audioPlayer.details.artist);
 
 
 const {song} = audioPlayer;
 const {song: anotherSong} = audioPlayer;
 const {song: music, songDuration: duration} = audioPlayer;
-console.log('Song: ', song);
-console.log('Song: ', anotherSong);
-console.log('Song: ', duration);
+// console.log('Song: ', song);
+// console.log('Song: ', anotherSong);
+// console.log('Song: ', duration);
 
 
 const {artist:author} = audioPlayer.details;
-console.log('Author: ()', author);
+// console.log('Author: ()', author);
+
+const dbz: string [] = ['Goku', 'Vegeta', 'Trunks'];
+console.log('personaje 3: ', dbz[2]);
+const trunk = dbz[3] || 'no personaje';
+console.log('personaje 3: ', trunk);
+const [p1,p2, trunks, gohan = 'not found'] = dbz;
+console.log('personaje (3): ', trunks);
+console.log('personaje (4): ', gohan);
+console.log('personaje (1): ', p1);
+
+
+export {};
