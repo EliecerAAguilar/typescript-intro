dbc8b3744ff8a9d308fc803018d6162af7ed70ee
Eliecer <eliecer.aguilar1@utp.ac.pa>
2024-06-11 22:50:04 -0500

    feat(topics)!: 26
    feat Hero class compose class

--- src/topics/08-classes.ts
+++ src/topics/08-classes.ts
@@ -6,16 +6,32 @@ export class Person {
 
     constructor(
         public name: string,
-        private address: string = 'no address'
+        private address: string = 'no address',
+        public lastName: string
     ){};
 }
 
-export class Hero extends Person {
-    constructor(public alterEgo: string,
-                public age: number,
-                public realName: string) {
-        super(realName,'Colombia');
+// export class Hero extends Person {
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string
+//     ) {
+//         super(realName,'Colombia');
+//     }
+// }
+
+export class Hero {
+    // public person: Person;
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName: string,
+        public person: Person
+    ) {
+        // this.person = new Person(realName);
     }
 }
-const ironMan = new Hero('IronMan',35, 'Tony');
+const eliecer: Person = new Person('Eliecer','', 'Aguilar');
+const ironMan = new Hero('IronMan',35, 'Tony', eliecer);
 console.log(ironMan);
